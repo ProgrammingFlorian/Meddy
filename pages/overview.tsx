914a75f3b23497af7f4dadd70556f597e95da483
Florian <ge95kek@mytum.de>
2023-02-04 02:17:36 +0100

    Started working on supabase integration

--- pages/overview.tsx
+++ pages/overview.tsx
@@ -5,21 +5,24 @@ const Overview: NextPage = () => {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
             <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-                <DndList data={[{
-                    id: 1,
-                    name: "Hufenstein",
-                    duration: 5
-                },
-                {
-                    id: 2,
-                    name: "Albert",
-                    duration: 50
-                },
-                {
-                    id: 3,
-                    name: "Simon",
-                    duration: 3
-                }]}></DndList>
+                <div>
+                    <h1 className="my-5">Dr. Proman</h1>
+                    <DndList data={[{
+                        id: 1,
+                        name: "Hufenstein",
+                        duration: 5
+                    },
+                    {
+                        id: 2,
+                        name: "Albert",
+                        duration: 50
+                    },
+                    {
+                        id: 3,
+                        name: "Simon",
+                        duration: 3
+                    }]}></DndList>
+                </div>
             </main>
         </div>
     )
