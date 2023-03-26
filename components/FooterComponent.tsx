948c1f7105787a517904b0fd4641ad73cb9908e3
Florian <ge95kek@mytum.de>
2023-03-27 00:12:10 +0200

    Fixed footer Name

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,11 +1,13 @@
 import {Text} from "@mantine/core";
+import {useTranslation} from "next-i18next";
 
-const FooterComponent = () => (
-    <footer className="relative h-24 w-full items-center justify-center border-t">
+const FooterComponent = () => {
+    const {t} = useTranslation();
+    return <footer className="relative h-24 w-full items-center justify-center border-t">
         <Text className="mt-5 mb-2" variant="gradient"
               gradient={{from: 'indigo', to: 'cyan', deg: 45}}
               sx={{fontFamily: 'Greycliff CF, sans-serif'}}
-              ta="center" fw={700}>Swait</Text>
+              ta="center" fw={700}>{t('appName')}</Text>
         <Text align="center">
             Webseite erstellt von{' '}
             <span className="font-bold">
@@ -13,6 +15,6 @@ const FooterComponent = () => (
             </span>
         </Text>
     </footer>
-);
+};
 
 export default FooterComponent;
