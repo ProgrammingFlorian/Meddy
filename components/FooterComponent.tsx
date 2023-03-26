3edb8dc56b9ae158e5a4f29013b705a5281bc28f
Florian <ge95kek@mytum.de>
2023-03-27 00:13:15 +0200

    Fixed footer Name

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,18 +1,23 @@
 import {Text} from "@mantine/core";
+import {useTranslation} from "next-i18next";
 
-const FooterComponent = () => (
-    <footer className="relative h-24 w-full items-center justify-center border-t">
-        <Text className="mt-5 mb-2" variant="gradient"
-              gradient={{from: 'indigo', to: 'cyan', deg: 45}}
-              sx={{fontFamily: 'Greycliff CF, sans-serif'}}
-              ta="center" fw={700}>Swait</Text>
-        <Text align="center">
-            Webseite erstellt von{' '}
-            <span className="font-bold">
+const FooterComponent = () => {
+    const {t} = useTranslation();
+
+    return (
+        <footer className="relative h-24 w-full items-center justify-center border-t">
+            <Text className="mt-5 mb-2" variant="gradient"
+                  gradient={{from: 'indigo', to: 'cyan', deg: 45}}
+                  sx={{fontFamily: 'Greycliff CF, sans-serif'}}
+                  ta="center" fw={700}>{t('appName')}</Text>
+            <Text align="center">
+                Webseite erstellt von{' '}
+                <span className="font-bold">
                 Florian Koller & Marius Weigt
             </span>
-        </Text>
-    </footer>
-);
+            </Text>
+        </footer>
+    );
+};
 
 export default FooterComponent;
